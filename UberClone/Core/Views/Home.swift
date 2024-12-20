f245327a4acbe6ad45a450643a6e88c697978f9e
Lynn Do <do24l@mtholyoke.edu>
2024-12-20 10:47:18 -0800

    Initial setup

---
+++ UberClone/Core/Views/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  UberClone
+//
+//  Created by Do Linh on 12/20/24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Home()
+}
