ba5ba52f32346e5788f15e68e751e53ae4343dab
Lynn Do <do24l@mtholyoke.edu>
2024-12-20 10:13:54 -0800

    Initial Commit

---
+++ UberClone/UberCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneApp.swift
+//  UberClone
+//
+//  Created by Do Linh on 12/20/24.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UberCloneTests/UberCloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneTests.swift
+//  UberCloneTests
+//
+//  Created by Do Linh on 12/20/24.
+//
+
+import Testing
+@testable import UberClone
+
+struct UberCloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
